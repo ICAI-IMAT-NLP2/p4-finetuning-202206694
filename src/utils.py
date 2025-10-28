a7eafbe75ae1bd6d7dfb52d991f422c71b90af4b
202206694 <202206694@alu.comillas.edu>
2025-10-28 17:20:08 +0100

    codigo py

--- src/utils.py
+++ src/utils.py
@@ -14,9 +14,13 @@ def download_and_load_model(model_name="t5-small", device="cuda" if torch.cuda.i
         model (PreTrainedModel): The loaded T5-small model.
         tokenizer (PreTrainedTokenizer): The corresponding tokenizer.
     """
+    if device is None:
+        device = "cuda" if torch.cuda.is_available() else "cpu"
     print(f"Downloading and loading the {model_name} model...")
     tokenizer = T5Tokenizer.from_pretrained(model_name)
     model = T5ForConditionalGeneration.from_pretrained(model_name)
     model.to(device)
     
     return model, tokenizer, device
+
+
